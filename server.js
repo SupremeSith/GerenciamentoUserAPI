0d236087ee6206fc2a9458b8a1358d16f984029f
SupremeSith <woof.portocarrero@gmail.com>
2025-04-04 18:52:51 -0300

    configuraçõa do get/listagem no ambiente de usuarios

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
 
 
 // post salva os usuarios
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios',async (req, res) => {
 
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+
+
 // o body é o que vem no corpo da requisição, ou seja, o que o cliente envia para o servidor
 // o push adiciona o usuario no array de usuarios
 // o status 201 indica que o recurso foi criado com sucesso
@@ -20,8 +30,17 @@ app.post('/usuarios', (req, res) => {
 // get retorna os usuarios
 // o get é usado para obter informações do servidor
 // o status 200 indica que a requisição foi bem sucedida
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios',async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+
     res.status(200).json(users)
 })
 
 app.listen(3000)
+
+
+
+//usuario rodolfo
+// senha S4A3jnQTxpPYeedZ
