8e0e30cabbcf607babcd1a670b81d840b3e8d0e0
SupremeSith <woof.portocarrero@gmail.com>
2025-04-06 19:52:03 -0300

    adicionar cadastro de usuários via formulário e listar automaticamente após envio

--- interface/cadastro-usuario/src/pages/Home/index.jsx
+++ interface/cadastro-usuario/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './style.css';
 import Trash from '../../assets/trash.svg';
 import api from '../../services/api';
@@ -6,9 +6,25 @@ import api from '../../services/api';
 function Home() {
   const [users, setUsers] = useState([]);
 
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+
+
   async function getUsers() {
     const usersFromApi = await api.get('/usuarios');
     setUsers(usersFromApi.data);
+  }
+
+  async function createUsers() {
+    await api.post('/usuarios',
+      {
+        name: inputName.current.value,
+        age: inputAge.current.value,
+        email: inputEmail.current.value
+      }
+    )
   }
   
 
@@ -22,10 +38,10 @@ function Home() {
     <div className="container">
       <form>
         <h1>Cadastro usuarios</h1>
-        <input name="nome" type="text" placeholder="Nome" />
-        <input name="idade" type="number" placeholder="Idade" />
-        <input name="email" type="email" placeholder="Email" />
-        <button type="button">Cadastrar</button>
+        <input name="nome" type="text" placeholder="Nome" ref={inputName} />
+        <input name="idade" type="number" placeholder="Idade" ref={inputAge} />
+        <input name="email" type="email" placeholder="Email" ref={inputEmail}/>
+        <button type="button" onClick={createUsers}>Cadastrar</button>
       </form>
       <div className="users-container">
         {users.map((user) => (
